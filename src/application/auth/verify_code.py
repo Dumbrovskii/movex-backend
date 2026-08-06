f93f1b33aadef94082c629a06b2af552af7a9a5e
Dumbrovskii <dumbrovskyy@gmail.com>
2026-08-06 22:07:21 +0300

    Updated verification code use case

--- src/application/auth/verify_code.py
+++ src/application/auth/verify_code.py
@@ -5,7 +5,13 @@ from src.application.exceptions import (
     InvalidVerificationCodeError,
 )
 
-from src.application.interfaces import VerificationCodeRepository
+from src.application.interfaces import (
+    VerificationCodeRepository,
+    RefreshTokenRepository,
+    JwtService,
+    RefreshTokenService,
+)
+from src.config import settings
 
 
 @dataclass(slots=True, frozen=True)
@@ -18,15 +24,21 @@ class VerifyCodeUseCase:
 
     def __init__(
             self,
-            repository: VerificationCodeRepository,
+            verification_code_repository: VerificationCodeRepository,
+            refresh_token_repository: RefreshTokenRepository,
+            jwt_service: JwtService,
+            refresh_token_service: RefreshTokenService
     ) -> None:
-        self._repository = repository
+        self._verification_code_repository = verification_code_repository
+        self._refresh_token_repository = refresh_token_repository
+        self._jwt_service = jwt_service
+        self._refresh_token_service = refresh_token_service
 
     async def execute(
             self,
             command: VerifyCodeCommand,
     ) -> tuple[str, str]:
-        stored_code = await self._repository.get_code(
+        stored_code = await self._verification_code_repository.get_code(
             command.phone,
         )
 
@@ -36,12 +48,23 @@ class VerifyCodeUseCase:
         if stored_code != command.code:
             raise InvalidVerificationCodeError()
 
-        await self._repository.delete_code(
-            command.phone,
+        user_id = 1
+
+        access_token = self._jwt_service.create_access_token(
+            user_id=user_id,
         )
 
-        access_token = "access-token"
-        refresh_token = "refresh-token"
+        refresh_token = self._refresh_token_service.generate()
+
+        await self._refresh_token_repository.save(
+            token=refresh_token,
+            user_id=user_id,
+            ttl_seconds=settings.REFRESH_TOKEN_TTL_DAYS * 24 * 60 * 60,
+        )
+
+        await self._verification_code_repository.delete_code(
+            command.phone,
+        )
 
         return (
             access_token,
