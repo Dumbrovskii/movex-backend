7f4c67de46efd2d37258f791c001ad01fc33fb12
Dumbrovskii <dumbrovskyy@gmail.com>
2026-08-06 22:30:42 +0300

    Update __init__.py

--- src/application/interfaces/repositories/__init__.py
+++ src/application/interfaces/repositories/__init__.py
@@ -1,7 +1,9 @@
 from .verification_code_repository import VerificationCodeRepository
 from .refresh_token_repository import RefreshTokenRepository
+from .user_repository import UserRepository
 
 __all__ = [
     "VerificationCodeRepository",
     "RefreshTokenRepository",
+    "UserRepository",
 ]
