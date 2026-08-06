e8f3dfbc7c1edab21fb24342bf42f0f249cd7416
Dumbrovskii <dumbrovskyy@gmail.com>
2026-08-06 22:06:37 +0300

    Update __init__.py

--- src/infrastructure/redis/__init__.py
+++ src/infrastructure/redis/__init__.py
@@ -1,7 +1,9 @@
 from .client import redis
 from .verification_code_repository import RedisVerificationCodeRepository
+from .refresh_token_repository import RedisRefreshTokenRepository
 
 __all__ = [
     "redis",
     "RedisVerificationCodeRepository",
+    "RedisRefreshTokenRepository",
 ]
