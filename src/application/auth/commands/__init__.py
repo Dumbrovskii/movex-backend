aa4e6cc0d644e5456dc8c64395b2c61782ef5c2c
Dumbrovskii <dumbrovskyy@gmail.com>
2026-08-07 19:16:39 +0300

    Update __init__.py

--- src/application/auth/commands/__init__.py
+++ src/application/auth/commands/__init__.py
@@ -1,9 +1,11 @@
 from .request_code import RequestCodeCommand
 from .refresh_token import RefreshTokenCommand
 from .verify_code import VerifyCodeCommand
+from .logout import LogoutCommand
 
 __all__ = [
     "RequestCodeCommand",
     "RefreshTokenCommand",
     "VerifyCodeCommand",
+    "LogoutCommand",
 ]
