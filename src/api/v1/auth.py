4cb486604c0aa89a62d7b897e3f0084bcde4d5af
Dumbrovskii <dumbrovskyy@gmail.com>
2026-08-07 19:08:11 +0300

    Add rout for refresh token

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -5,16 +5,23 @@ from src.api.contracts.auth import (
     RequestCodeResponse,
     VerifyCodeRequest,
     VerifyCodeResponse,
+    RefreshTokenRequest,
+    RefreshTokenResponse,
 )
-from src.application.auth import (
+from src.application.auth.commands import (
     RequestCodeCommand,
-    RequestCodeUseCase,
     VerifyCodeCommand,
+    RefreshTokenCommand,
+)
+from src.application.auth.use_cases import (
+    RequestCodeUseCase,
     VerifyCodeUseCase,
+    RefreshTokenUseCase,
 )
 from src.api.dependencies.auth import (
     get_request_code_use_case,
-    get_verify_code_use_case
+    get_verify_code_use_case,
+    get_refresh_token_use_case,
 )
 
 
@@ -48,9 +55,7 @@ async def request_code(
 )
 async def verify_code(
         request: VerifyCodeRequest,
-        use_case: VerifyCodeUseCase = Depends(
-            get_verify_code_use_case,
-        ),
+        use_case: VerifyCodeUseCase = Depends(get_verify_code_use_case),
 ):
     command = VerifyCodeCommand(
         phone=request.phone,
@@ -59,6 +64,25 @@ async def verify_code(
     access_token, refresh_token = await use_case.execute(command)
 
     return VerifyCodeResponse(
+        access_token=access_token,
+        refresh_token=refresh_token,
+    )
+
+@router.post(
+    "/refresh",
+    response_model=RefreshTokenResponse,
+)
+async def refresh_token(
+        request: RefreshTokenRequest,
+        use_case: RefreshTokenUseCase = Depends(get_refresh_token_use_case),
+):
+    command = RefreshTokenCommand(
+        refresh_token=request.refresh_token,
+    )
+
+    access_token, refresh_token = await use_case.execute(command)
+
+    return RefreshTokenResponse(
         access_token=access_token,
         refresh_token=refresh_token,
     )
