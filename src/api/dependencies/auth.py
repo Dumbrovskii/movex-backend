8ecbd35ed9c6e329574e5785d3b384f48f77c01e
Dumbrovskii <dumbrovskyy@gmail.com>
2026-08-07 19:06:55 +0300

    add dependencies for refresh token

--- src/api/dependencies/auth.py
+++ src/api/dependencies/auth.py
@@ -1,37 +1,54 @@
-from src.application.auth import (
+from fastapi import Depends
+
+from src.application.auth.use_cases import (
     RequestCodeUseCase,
-    VerifyCodeUseCase
+    VerifyCodeUseCase,
+    RefreshTokenUseCase,
 )
-from src.infrastructure.redis import (
-    RedisVerificationCodeRepository,
-    RedisRefreshTokenRepository,
-    redis,
+from src.application.interfaces import (
+    SmsSender,
+    TokenService,
+    UserRepository,
+    VerificationCodeRepository,
+    RefreshTokenRepository,
 )
-from src.infrastructure.security import (
-    PyJwtService,
-    DefaultRefreshTokenService
+
+from .providers import (
+    get_token_service,
+    get_user_repository,
+    get_sms_sender,
+    get_verification_code_repository,
+    get_refresh_token_repository,
 )
-from src.infrastructure.sms import DummySmsSender
 
 
-def get_request_code_use_case() -> RequestCodeUseCase:
-    repository = RedisVerificationCodeRepository(redis)
-    sms_sender = DummySmsSender()
-
+def get_request_code_use_case(
+        verification_code_repository: VerificationCodeRepository = Depends(get_verification_code_repository),
+        sms_sender: SmsSender = Depends(get_sms_sender),
+) -> RequestCodeUseCase:
     return RequestCodeUseCase(
-        repository=repository,
+        repository=verification_code_repository,
         sms_sender=sms_sender,
     )
 
-def get_verify_code_use_case() -> VerifyCodeUseCase:
-    verification_code_repository = RedisVerificationCodeRepository(redis)
-    refresh_token_repository = RedisRefreshTokenRepository(redis)
-    jwt_service = PyJwtService()
-    refresh_token_service = DefaultRefreshTokenService()
+async def get_verify_code_use_case(
+        verification_code_repository: VerificationCodeRepository = Depends(get_verification_code_repository),
+        token_service: TokenService = Depends(get_token_service),
+        user_repository: UserRepository = Depends(get_user_repository),
+) -> VerifyCodeUseCase:
 
     return VerifyCodeUseCase(
         verification_code_repository=verification_code_repository,
+        token_service=token_service,
+        user_repository=user_repository,
+    )
+
+async def get_refresh_token_use_case(
+        refresh_token_repository: RefreshTokenRepository = Depends(get_refresh_token_repository),
+        token_service: TokenService = Depends(get_token_service)
+) -> RefreshTokenUseCase:
+
+    return RefreshTokenUseCase(
         refresh_token_repository=refresh_token_repository,
-        jwt_service=jwt_service,
-        refresh_token_service=refresh_token_service,
+        token_service=token_service,
     )
---
+++ src/api/dependencies/providers.py
@@ -0,0 +1,56 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.application.interfaces import (
+    JwtService,
+    RefreshTokenService,
+    SmsSender,
+    RefreshTokenRepository,
+    UserRepository,
+    VerificationCodeRepository,
+    TokenService,
+)
+from src.infrastructure.db.repositories import SqlAlchemyUserRepository
+from src.infrastructure.db.session import get_session
+from src.infrastructure.redis import (
+    RedisVerificationCodeRepository,
+    RedisRefreshTokenRepository,
+    redis,
+)
+from src.infrastructure.security import (
+    PyJwtService,
+    DefaultRefreshTokenService,
+    DefaultTokenService,
+)
+
+from src.infrastructure.sms import DummySmsSender
+
+
+def get_verification_code_repository() -> VerificationCodeRepository:
+    return RedisVerificationCodeRepository(redis)
+
+def get_refresh_token_repository() -> RefreshTokenRepository:
+    return RedisRefreshTokenRepository(redis)
+
+async def get_user_repository( session: AsyncSession = Depends(get_session),) -> UserRepository:
+    return SqlAlchemyUserRepository(session)
+
+def get_sms_sender() -> SmsSender:
+    return DummySmsSender()
+
+def get_jwt_service() -> JwtService:
+    return PyJwtService()
+
+def get_refresh_token_service() -> RefreshTokenService:
+    return DefaultRefreshTokenService()
+
+def get_token_service(
+        jwt_service: JwtService = Depends(get_jwt_service),
+        refresh_token_service: RefreshTokenService = Depends(get_refresh_token_service),
+        refresh_token_repository: RefreshTokenRepository = Depends(get_refresh_token_repository),
+) -> TokenService:
+    return DefaultTokenService(
+        jwt_service=jwt_service,
+        refresh_token_service=refresh_token_service,
+        refresh_token_repository=refresh_token_repository,
+    )
