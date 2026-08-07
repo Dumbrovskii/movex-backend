90cdaa225c1ebcc548649a1adc9192dd8ed3340d
Dumbrovskii <dumbrovskyy@gmail.com>
2026-08-07 19:07:02 +0300

    Update __init__.py

--- src/api/dependencies/__init__.py
+++ src/api/dependencies/__init__.py
@@ -1,9 +1,11 @@
 from .auth import (
     get_request_code_use_case,
     get_verify_code_use_case,
+    get_refresh_token_use_case,
 )
 
 __all__ = [
     "get_request_code_use_case",
     "get_verify_code_use_case",
+    "get_refresh_token_use_case",
 ]
