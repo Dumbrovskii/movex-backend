37255481c40d3cc9da229f7f9da7d4af69784860
Dumbrovskii <dumbrovskyy@gmail.com>
2026-09-16 23:45:00 +0300

    Update ride_status.py

--- src/domain/enums/ride_status.py
+++ src/domain/enums/ride_status.py
@@ -3,6 +3,7 @@ from enum import Enum, auto
 class RideStatus(Enum):
     REQUESTED = auto()
     ACCEPTED = auto()
+    DRIVER_ARRIVING = auto()
     IN_PROGRESS = auto()
     COMPLETED = auto()
     CANCELED = auto()
