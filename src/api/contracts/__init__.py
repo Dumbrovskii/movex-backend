cc1ce8e9466e3b3801f33544d8c560b4506b6f86
Dumbrovskii <dumbrovskyy@gmail.com>
2026-09-21 01:16:13 +0300

    add ride request contracts

--- src/api/contracts/__init__.py
+++ src/api/contracts/__init__.py
@@ -10,6 +10,8 @@ from .auth import (
 from .rides import (
     EstimateRideRequest,
     EstimateRideResponse,
+    RideRequest,
+    RideResponse,
 )
 
 __all__ = [
@@ -22,4 +24,6 @@ __all__ = [
     "LogoutRequest",
     "EstimateRideRequest",
     "EstimateRideResponse",
+    "RideRequest",
+    "RideResponse",
 ]
